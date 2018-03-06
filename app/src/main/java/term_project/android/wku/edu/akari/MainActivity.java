a184f609c8938b985b56887e356690b886b30df3
mbutera11 <michael.butera11@gmail.com>
2018-03-06 16:12:18 -0600

    Start App Creation

---
+++ app/src/main/java/term_project/android/wku/edu/akari/MainActivity.java
@@ -0,0 +1,13 @@
+package term_project.android.wku.edu.akari;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
