2559db2b09f288aeb9582b0a84d93ff028363da7
tomtzs <36006134+tomtzs@users.noreply.github.com>
2018-03-18 16:28:01 -0500

    Menus and pages added

---
+++ app/src/main/java/term_project/android/wku/edu/akari/LoginLogout.java
@@ -0,0 +1,13 @@
+package term_project.android.wku.edu.akari;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginLogout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_logout);
+    }
+}
--- app/src/main/java/term_project/android/wku/edu/akari/MainActivity.java
+++ app/src/main/java/term_project/android/wku/edu/akari/MainActivity.java
@@ -1,7 +1,11 @@
 package term_project.android.wku.edu.akari;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.listings:
+                Intent intent = new Intent(this, Listings.class);
+                this.startActivity(intent);
+                break;
+            case R.id.properties:
+                intent = new Intent(this, Properties.class);
+                this.startActivity(intent);
+                break;
+            case R.id.profile:
+                intent = new Intent(this, Profile.class);
+                this.startActivity(intent);
+                break;
+            case R.id.login:
+                intent = new Intent(this, LoginLogout.class);
+                this.startActivity(intent);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+        public boolean onCreateOptionsMenu(Menu menu) {
+
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menu, menu);
+            return true;
+        }
+
 }
