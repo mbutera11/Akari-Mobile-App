bb090e53b37c18a5f566885923ac081e2b1391ba
tomtzs <36006134+tomtzs@users.noreply.github.com>
2018-03-19 21:45:48 -0500

    added profile, post property, and property page

--- app/src/main/java/term_project/android/wku/edu/akari/Profile.java
+++ app/src/main/java/term_project/android/wku/edu/akari/Profile.java
@@ -1,7 +1,10 @@
 package term_project.android.wku.edu.akari;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Profile extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Button properties = (Button)findViewById(R.id.properties);
+        Button postProperty = (Button)findViewById(R.id.postProperty);
+
+        properties.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Profile.this, Properties.class));
+            }
+        });
+        postProperty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Profile.this, postProperty.class));
+            }
+        });
     }
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+
 }
--- app/src/main/java/term_project/android/wku/edu/akari/postProperty.java
+++ app/src/main/java/term_project/android/wku/edu/akari/postProperty.java
@@ -3,13 +3,12 @@ package term_project.android.wku.edu.akari;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Properties extends AppCompatActivity {
+public class postProperty extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_properties);
-        getSupportActionBar().setTitle("Your Properties");
+        setContentView(R.layout.activity_post_property);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
     @Override
