1098d231d729859d4e2bf2a3dfe6b003fc118d14
mbutera11 <michael.butera11@gmail.com>
2018-03-29 17:15:41 -0500

    Established connection to MySQL server
    Login functionality works with existing mysql database. Requires a separate script

--- app/src/main/java/term_project/android/wku/edu/akari/Login.java
+++ app/src/main/java/term_project/android/wku/edu/akari/Login.java
@@ -1,9 +1,29 @@
 package term_project.android.wku.edu.akari;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class Login extends AppCompatActivity {
+
+    protected EditText email;
+    protected EditText password;
+    protected Button loginButton;
+    protected TextView firstName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +31,18 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         getSupportActionBar().setTitle("Login");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+    }
+
+    // method called on button click to log in
+    public void login(View view) {
+        String emailText = email.getText().toString();
+        String passwordText = password.getText().toString();
+        // new instance of the subclass, execute it
+        new LoginAction(this).execute(emailText, passwordText);
     }
 
     @Override
@@ -18,4 +50,84 @@ public class Login extends AppCompatActivity {
         finish();
         return true;
     }
+
+
+    // subclass that executed the login script on the server via POST
+    private class LoginAction extends AsyncTask<String, Integer, String > {
+
+        private Context context;
+
+        public LoginAction(Context context) {
+            this.context = context;
+        }
+
+        protected void onPreExecute() {
+        }
+
+        // communication happens here
+        @Override
+        protected String doInBackground(String... arg0) {
+            try{
+                // get email and password from when it is executed in the login method
+                String email = arg0[0];
+                String password = arg0[1];
+
+                // link to the script
+                String link="http://10.0.1.38/Code/Mobile_App_Scripts/loginMobile.php";
+
+                // encode the email and password to pass to the script via POSt
+                String data  = URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8");
+                data += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+
+                // create a URL with the link
+                URL url = new URL(link);
+                URLConnection conn = url.openConnection();
+                conn.setDoOutput(true);
+
+                // create writer to write encoded data to the URL
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                wr.write( data );
+                wr.flush();
+
+                // will read information received from the server
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                // Read Server Response and append to string builder
+                while((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                // return string
+                return sb.toString();
+
+            } catch(Exception e){
+                return new String("Exception: " + e.getMessage());
+            }
+        }
+
+        // called after background task is complete
+        // will take appropriate actions depending on the server response
+        @Override
+        protected void onPostExecute(String result) {
+            // toast email not registered if that is the script output
+            if(result.equals("Email is not Registered")) {
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+
+            // toast password incorrect if that is the script output
+            } else if(result.equals("Password is Incorrect")) {
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+
+            // if email is registered and password is correct, login is successful
+            // send user to profile page
+            } else {
+                Intent in = new Intent(getApplicationContext(), Profile.class);
+                in.putExtra("FirstName", result);
+                startActivity(in);
+            }
+        }
+    }
+
 }
--- app/src/main/java/term_project/android/wku/edu/akari/Profile.java
+++ app/src/main/java/term_project/android/wku/edu/akari/Profile.java
@@ -5,16 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
+
+    protected Button properties;
+    protected Button postProperty;
+    protected TextView firstName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        Button properties = (Button)findViewById(R.id.properties);
-        Button postProperty = (Button)findViewById(R.id.postProperty);
+        getSupportActionBar().setTitle("Your Profile");
+
+        properties = findViewById(R.id.properties);
+        postProperty = findViewById(R.id.postProperty);
+
+        // testing login with this--------------------
+        firstName = findViewById(R.id.firstName);
+        firstName.setText(getIntent().getStringExtra("FirstName"));
 
         properties.setOnClickListener(new View.OnClickListener() {
             @Override
